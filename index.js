84793dc6e99bbd36a8aedd188057bd15f7281d4d
Steven Huang <96713218+huangs-nyc@users.noreply.github.com>
2023-06-22 14:18:28 -0400

    Completed

--- index.js
+++ index.js
@@ -6,47 +6,52 @@ let homeScore = 0
 let guestScore = 0
 let periodCount = 1
 
-function homeIncrement() {
-    homeScore += 1
-    homeEl.textContent = homeScore
+let resetInput = ''
+let count = 0
+let group = ''
+
+function increment(group, count) {
+    if (group === 'h') {
+        homeScore += count
+        homeEl.textContent = homeScore
+    } else if (group === 'g') {
+        guestScore += count
+        guestEl.textContent = guestScore
+    } else {
+        if (periodCount+1 > 4) {
+            periodCount = 5
+            periodEl.textContent = "OVR"
+        } else {
+            periodCount += count
+            periodEl.textContent = periodCount
+        }
+    }
 }
 
-function guestIncrement() {
-    guestScore += 1
-    guestEl.textContent = guestScore
+function decrease(group, count) {
+    if (group === 'h') {
+        homeScore -= count
+        homeEl.textContent = homeScore
+    } else if (group === 'g') {
+        guestScore -= count
+        guestEl.textContent = guestScore
+    } else {
+        if (periodCount-1 > 0) {
+            periodCount -= count
+            periodEl.textContent = periodCount
+        }
+    }
 }
 
-function periodIncrement() {
-    periodCount += 1
-    periodEl.textContent = periodCount
-}
-
-function homeDecrease() {
-    homeScore -= 1
-    homeEl.textContent = homeScore
-}
-
-function guestDecrease() {
-    guestScore -= 1
-    guestEl.textContent = guestScore
-}
-
-function periodDecrease() {
-    periodCount -= 1
-    periodEl.textContent = periodCount
-}
-
-function homeReset() {
-    homeScore = 0
-    homeEl.textContent = homeScore
-}
-
-function guestReset() {
-    guestScore = 0
-    guestEl.textContent = guestScore
-}
-
-function periodReset() {
-    periodCount = 1
-    periodEl.textContent = periodCount
+function reset(resetInput) {
+    if (resetInput === 'h') {
+        homeScore = 0
+        homeEl.textContent = homeScore
+    } else if (resetInput === 'p') {
+        periodCount = 1
+        periodEl.textContent = periodCount
+    } else {
+        guestScore = 0
+        guestEl.textContent = guestScore
+    }
 }
